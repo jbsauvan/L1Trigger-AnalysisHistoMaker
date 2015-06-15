6bc91b6aaad658663441d5a6c6711309f12e766b
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2015-06-15 16:29:31 +0200

    Update rate config file

--- ZeroBias_Rate/run/batch/ZeroBias_Rate_NuGun_13TeV_PU40bx25_Trimming5.py
+++ ZeroBias_Rate/run/batch/ZeroBias_Rate_NuGun_13TeV_PU40bx25_Trimming5.py
@@ -1,10 +1,15 @@
 from AnhimaBatchLauncher import AnhimaBatchLauncher
 import glob
 
+baseDir = os.environ["ANHIMA_BASE"]
+cmsswDir = os.environ["CMSSW_BASE"] 
+wrapperDir = os.environ["EMUL_WRAPPER_BASE"]
+
 inputBaseDir = "/home/llr/cms/sauvan/DATA/L1/Ntuples/EGammaStage2"
 inputTemplate = "v_4_2014-10-10/tree_electron_stage2_*_0.root"
 datasets = []
 datasets.append("Neutrino_Pt-2to20_gun/Fall13dr-tsg_PU40bx25_POSTLS162_V2-v1")
+
 
 
 inputFiles =  []
@@ -13,7 +18,7 @@ for dataset in datasets:
 
 
 batch = AnhimaBatchLauncher()
-batch.exe = "/home/llr/cms/sauvan/L1_upgrade/AnHiMa2/ZeroBias_Rate/analysis.exe"
+batch.exe = "{}/ZeroBias_Rate/analysis.exe".format(baseDir)
 batch.name = "zeroBias_rate_NuGun_13TeV_PU40bx25"
 batch.inputFiles = inputFiles
 batch.tree = "electronNtuplizer_electronTree"
@@ -21,31 +26,27 @@ batch.outputDirectory = "/data_CMS/cms/sauvan/L1/EmulatorEra/ZeroBias/rate/NuGun
 batch.outputFile = "zeroBias_rate_NuGun_13TeV_PU40bx25_Trimming5.root"
 batch.histoParameters = "../histos.par"
 batch.histoTag = "Histos"
-batch.nFilesPerJob = 1
+batch.nFilesPerJob = 2
 batch.queue = "cms"
-batch.cmsswDir = "/home/llr/cms/sauvan/CMSSW/L1_Emulator/CMSSW_7_3_0_pre1/"
+batch.cmsswDir = cmsswDir
 batch.additionalSetup = """## setup emulator wrapper
-cd /home/llr/cms/sauvan/L1_upgrade/L1TEmulatorWrapper/
+cd {0}
 source wrapenv
 cd -
-ln -s /home/llr/cms/sauvan/L1_upgrade/AnHiMa2/TriggerCommon/ ../
-"""
+ln -s {1}/TriggerCommon/ ../
+""".format(wrapperDir, baseDir)
 
 #batch.local = True
 
 #batch.cuts = [""]
 
-batch.additionalParameters = {"EmulatorParameters": "/home/llr/cms/sauvan/L1_upgrade/L1TEmulatorWrapper/data/caloStage2ParamsTrimming5.txt",
-                              "CutsHoEFile": "/home/llr/cms/sauvan/L1_upgrade/AnHiMa2/TagAndProbe_Identification/data/cutsHoE_14.10.09.root",
+batch.additionalParameters = {"EmulatorParameters": "{}/data/caloStage2ParamsTrimming5.txt".format(wrapperDir),
+                              "CutsHoEFile": "{}/TriggerCommon/data/cutsHoE_14.10.09.root".format(baseDir),
                               "CutsHoEHisto": "cutsEff05_eta_ET",
-                              "ShapeDiscriminantFile": "/home/llr/cms/sauvan/L1_upgrade/AnHiMa2/TagAndProbe_Identification/data/mergedDiscriminant_eta_et_shape.root",
+                              "ShapeDiscriminantFile": "{}/TriggerCommon/data/mergedDiscriminant_eta_et_shape.root".format(baseDir),
                               "ShapeDiscriminantHisto": "shapeDiscriminant2",
-                              "ShapeWorkingPoint":"0.9",
-                              "ApplyCorr95":"True",
-                              "Corr95EtaStage2File":"/home/llr/cms/sauvan/L1_upgrade/AnHiMa2/TagAndProbe_Resolution/data/corr_vs_eta_Stage2_95pcEff.txt",
-                              "Corr95EtaStage1File":"/home/llr/cms/sauvan/L1_upgrade/AnHiMa2/TagAndProbe_Resolution/data/corr_vs_eta_Stage1_95pcEff_Emulator_1.5.txt"
+                              "ShapeWorkingPoint":"0.995",
+                              "ApplyCorr95":"False",
+                              "Corr95EtaStage2File":"{}/TriggerCommon/data/corr_vs_eta_Stage2_95pcEff.txt".format(baseDir),
+                              "Corr95EtaStage1File":"{}/TriggerCommon/data/corr_vs_eta_Stage1_95pcEff_Emulator_1.5.txt".format(baseDir)
                              }
-
-
-
-
